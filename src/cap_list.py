8fccc94097b72eb98e8af9087ce26c41ff578ffe
Noam <149065490+Noam-dv@users.noreply.github.com>
2025-11-05 21:16:27 +0200

    infinite packet display
    fix simple bug that was hard capping our packets being displayed
    packets were to get remove from the top once the cap was hit but the cap was never hit due to the deque limit being lower than the limit in the gui

--- src/cap_list.py
+++ src/cap_list.py
@@ -2,7 +2,10 @@ from collections import deque
 # file for publically acsesing the list of packets type shit
 # made this when starting project, thought it would be way more complex so i thought this could help organization
 # but ill just leaev it cuz it works
-ls = deque(maxlen=1000)
+
+
+#remove deque hard cap to fix bug in packet display
+ls = deque()
 
 def cap_list():
     return ls

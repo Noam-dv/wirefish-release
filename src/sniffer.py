5c0210d6859bb9e5aa293845831d54c35e5f7a34
Noam <149065490+Noam-dv@users.noreply.github.com>
2026-01-31 01:57:23 +0200

    docstrings - sniffer

--- src/sniffer.py
+++ src/sniffer.py
@@ -3,32 +3,38 @@ from scapy.utils import PcapWriter
 from cap_list import cap_list
 
 class Sniffer:
+    """simple sniffer class using scapy. this feels like cheating"""
+    
     def __init__(self, filter_text="tcp or udp", iface=None):
         self.interface = iface
         if self.interface == None: 
             self.interface = conf.iface
-        print(f"interfaec is - {self.interface}")
+        print(f"interface is - {self.interface}")
         self.writer = PcapWriter("output.pcap", append=True, sync=True)
-        self.create_sniffer()
+        self.create_sniffer() 
         self.write = False
 
     def create_sniffer(self):
-        self.sniffer = AsyncSniffer( iface=self.interface, filter="tcp or udp",prn=self.on_packet,store=False)
+        self.sniffer = AsyncSniffer( iface=self.interface, filter="tcp or udp",prn=self.on_packet,store=False) # async sniffer to update logs at the same time
 
-    @staticmethod #js writing here for organization
+    @staticmethod 
     def ret_ifaces():
         return get_if_list()
-    @staticmethod #default interface
+        
+    @staticmethod
     def d_iface():
+        """return default iface"""
         return conf.iface
         
     def toggle_writer(self):
+        """toggle the writer"""
         self.write = not self.write
 
     def add_to_list(self,l,s):
         l.append(s)
 
     def on_packet(self,pkt):
+        """categorize and display correct format"""
         if IP in pkt:
             p = "TCP" if TCP in pkt else "UDP" if UDP in pkt else "OTHER"
             self.add_to_list(cap_list(), f"{p}  ||  {pkt[IP].src} => {pkt[IP].dst}    |   {pkt.summary()}")
